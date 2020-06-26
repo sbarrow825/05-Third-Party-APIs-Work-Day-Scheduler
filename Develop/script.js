f3960031da6ad8e0f85633910f6c7757e256939e
sbarrow825 <sbarrow825@berkeley.edu>
2020-06-25 20:20:23 -0700

    text now successfully gets saved to local storage for a given hour upon clicking stage and saved text remains when the page is refreshed

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,45 @@
+var savedText = []
+var NineAMText = $("#9am");
+var TenAMText = $("#10am");
+var ElevenAMText = $("#11am");
+var TwelvePMText = $("#12pm");
+var OnePMText = $("#1pm");
+var TwoPMText = $("#2pm");
+var ThreePMText = $("#3pm");
+var FourPMText = $("#4pm");
+var FivePMText = $("#5pm");
+var allHourTexts = [NineAMText, TenAMText, ElevenAMText, TwelvePMText, OnePMText, TwoPMText, ThreePMText, FourPMText, FivePMText];
+
+
 $(document).on("click", ".saveBtn", save);
 
 function save() {
-    console.log("testing");
+    var numberButtonPressed = $(this).attr("id");
+    numberButtonPressed = parseInt(numberButtonPressed);
+    // console.log(NineAMText.val());
+    // console.log(NineAMText.val() === "");
+    var thisHourText = allHourTexts[numberButtonPressed].val();
+    if (localStorage.getItem("savedSchedule")) {
+        var newSchedule = JSON.parse(localStorage.getItem("savedSchedule"));
+        newSchedule[numberButtonPressed] = thisHourText;
+        localStorage.setItem("savedSchedule", JSON.stringify(newSchedule));
+    } else {
+        newSchedule = ["","","","","","","","",""]
+        newSchedule[numberButtonPressed] = thisHourText;
+        localStorage.setItem("savedSchedule", JSON.stringify(newSchedule));
+    }
+    console.log(localStorage.getItem("savedSchedule"));
 }
+
+function setUpSchedule() {
+    if (!localStorage.getItem("savedSchedule")) {
+        return;
+    }
+    var currentSchedule = JSON.parse(localStorage.getItem("savedSchedule"));
+    for (i = 0; i < currentSchedule.length; i += 1) {
+        var hourText = currentSchedule[i];
+        allHourTexts[i].val(hourText);
+    }
+}
+
+setUpSchedule();
